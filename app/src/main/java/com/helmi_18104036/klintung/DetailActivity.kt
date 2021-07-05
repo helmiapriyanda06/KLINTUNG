58c6634d408639d1f2559c80272b662c76b573f5
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-07-05 08:33:27 +0700

    detail

---
+++ app/src/main/java/com/helmi_18104036/klintung/DetailActivity.kt
@@ -0,0 +1,27 @@
+package com.helmi_18104036.klintung
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.helmi_18104036.klintung.databinding.ActivityScrollingBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
