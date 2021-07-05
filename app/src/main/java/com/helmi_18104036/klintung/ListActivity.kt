0bc849ec38e7fb637d89fe94ceaf4ade49d2ecdf
helmiapriyanda <56975427+helmiapriyanda@users.noreply.github.com>
2021-07-05 08:58:17 +0700

    add

---
+++ app/src/main/java/com/helmi_18104036/klintung/ListActivity.kt
@@ -0,0 +1,11 @@
+package com.helmi_18104036.klintung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
